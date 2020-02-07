39b0de1036de8d2f4a4af4f51bf04e773876ef0b
Yuriy <ibarthezz@gmail.com>
2020-02-06 21:57:24 -0500

    выполнено задание 04_student.py

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -9,6 +9,20 @@
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
 
-educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+educational_grant, expenses = 10000, 12000
+difference = 0
+inflation = 0
+need_help = 0
+i = 0
+while i < 10:
+    i += 1
+    difference = expenses - educational_grant
+    inflation = expenses / 100 * 3
+    expenses = expenses + inflation
+    need_help = need_help + difference
+
+else:
+    print('Студенту надо попросить', need_help.__round__(2), 'рублей')
+
+
