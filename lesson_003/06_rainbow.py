096cb6f69deb7068bc25f1799d1189e21a63ef8a
Yuriy <ibarthezz@gmail.com>
2020-02-08 19:42:38 -0500

    не доконца выполнено задание 06_rainbow.py не понимую почему выдает ошибку

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -8,7 +8,14 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+sd.resolution = (1000, 500)
+start_point = (50, 50, 5)
+end_point = (350, 450, 5)
+width = 4
+for color in rainbow_colors:
+    sd.line(start_point=start_point, end_point=end_point, color=color, width=width)
+
+# не могу понять почему выдает ошибку AttributeError: 'tuple' object has no attribute 'to_screen'!!
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
