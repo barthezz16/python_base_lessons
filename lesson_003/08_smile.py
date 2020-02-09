ca70507797f258ed29da7a3bb35432f83e5be273
Yuriy <ibarthezz@gmail.com>
2020-02-09 09:53:45 -0500

    внес правки в 08_smile.py но все равно не получилось... похоже что то я не совсем понял по функциям.

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -3,6 +3,7 @@
 # (определение функций)
 import simple_draw as sd
 
+
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
@@ -11,17 +12,14 @@ import simple_draw as sd
 # TODO И точки для каждого элемента связать между собой через x и y
 # TODO Например если центр x, y, то глаз x + 10, y + 10 или что-то вроде этого
 # TODO Тогда можно будет передавать функции случайную точку, а вокруг неё отрисуются все элементы смайлика
-for _ in range(10):
-    start_position = sd.get_point(185, 280)
-    end_position = sd.get_point(210, 285)
+def smile(point):
+    point = sd.get_point(100, 100)
+    smile_start_x = 152
+    smile1_y = 270
+    smile_end_x = smile_start_x + 45
+    start_position = sd.get_point(smile_start_x, smile1_y)
+    end_position = sd.get_point(smile_end_x, smile1_y)
     sd.line(start_point=start_position, end_point=end_position)
-    start_position = sd.get_point(165, 280)
-    end_position = sd.get_point(185, 280)
-    sd.line(start_point=start_position, end_point=end_position)
-    start_position = sd.get_point(135, 285)
-    end_position = sd.get_point(165, 280)
-    sd.line(start_point=start_position, end_point=end_position)
-    point = sd.get_point(300, 300)
     radius = 10
     for x in range(150, 201, 50):
         point = sd.get_point(x, 300)
@@ -29,7 +27,12 @@ for _ in range(10):
         point = sd.get_point(175, 290)
         radius = 10
         sd.circle(center_position=point, radius=50, width=2)
+    smile(sd.random_point())
 
 
+for _ in range(10):
+    smile(point=sd.random_point())
+
+sd.pause()
 
 sd.pause()
