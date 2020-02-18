1fc02b33b1a0d3ef775c66e518fc96d6382e3a4d
Юрий Сорокин <ibarthezz@gmail.com>
2020-02-17 19:45:53 -0500

    выстрадал 04_fractal.py

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -3,6 +3,7 @@
 import simple_draw as sd
 
 sd.resolution = (1200, 800)
+sd.background_color = (255, 255, 255)
 
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
@@ -31,63 +32,28 @@ sd.resolution = (1200, 800)
 root_point = sd.get_point(600, 0)
 
 
-def draw_branches(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw()
-    return v1.end_point
-
-
-# def draw_branches(point, angle, length):
-#     if length < 1:
-#         return
-#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-#     v1.draw()
-#     angle_1 = angle + 30
-#     angle_2 = angle - 30
-#     v2 = sd.get_vector(start_point=point, angle=angle_1, length=length, width=3)
-#     v3 = sd.get_vector(start_point=point, angle=angle_2, length=length, width=3)
-#
-
-
-# Только там пример ограничивается одним вызовом функцией самой себя, а нам нужно будет 2 вызова
-# Чтобы мы передавали одну точку в два вызова функции с разными углами,
-# тогда из этой точки будет нарисовано 2 ветви
-# тут тоже вроде все на теории понятно, но как застваить функцию принимать последнии координаты линии не пойму,
-# особенно потом, когда линий будет все больше...
-# деф бранч(точка, угол, длина)
-#     условие остановки рекурсии (в лекции есть)
-#     рисование_вектора(точка, угол, длина)
-#     расчёт_новых_углов
-#     бранч(новая_точка, угол1, длина)
-#     бранч(новая_точка, угол2, длина)
-
 def branch(point, angle, length):
     if length < 1:
         return
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw()
-    angle_1 = angle + 30
-    angle_2 = angle - 30
-    # TODO Вместо векторов тут нужны вызовы функции branch (самой себя)
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle_1, length=length, width=3)
-    v2.draw()
-    v3 = sd.get_vector(start_point=v1.end_point, angle=angle_2, length=length, width=3)
-    v3.draw()
-    return v1.end_point  # TODO И ретурн не нужен будет
+    if 1 < length < 10:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
+        v1.draw(color=sd.COLOR_DARK_GREEN)
+    else:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
+        v1.draw(color=sd.COLOR_DARK_ORANGE)
+        next_length = length * .75
+        angle_1 = angle + 30
+        angle_2 = angle - 30
+        branch(point=v1.end_point, angle=angle_1, length=next_length)
+        branch(point=v1.end_point, angle=angle_2, length=next_length)
 
-
-angle_0 = 90
-length_0 = 200
-next_point = branch(point=root_point, angle=angle_0, length=length_0)
-first_angle = angle_0 - 30
-next_length = length_0 * .75
-first_point = branch(point=next_point, angle=first_angle, length=next_length)
-second_angle = angle_0 + 30
-second_point = branch(point=next_point, angle=second_angle, length=next_length)
 
 point_0 = sd.get_point(600, 0)
+angle_0 = 90
+length = 200
 
-# TODO тут уперся в это
+branch(point=point_0, angle=angle_0, length=length)
+
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
