189641f9d881db242b047307436e13da01e355b1
Юрий Сорокин <ibarthezz@gmail.com>
2020-02-18 19:29:32 -0500

    внес изменения в 02_global_color.py

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -18,64 +18,107 @@ sd.set_screen_size(900, 900)
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
 
-def triangle(triangle_point, angle=0):
-    first_line = sd.get_vector(start_point=triangle_point, angle=angle, length=200, width=3)
-    first_line.draw(color=colors[color_number])
+# def triangle(triangle_point, angle=0):
+#     first_line = sd.get_vector(start_point=triangle_point, angle=angle, length=200, width=3)
+#     first_line.draw(color=colors[color_number])
+#
+#     second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 120, length=200, width=3)
+#     second_line.draw(color=colors[color_number])
+#
+#     sd.line(start_point=second_line.end_point, end_point=triangle_point, width=3, color=colors[color_number])
+#
+#
+# def square(square_point, angle=0):
+#     first_line = sd.get_vector(start_point=square_point, angle=0, length=200, width=3)
+#     first_line.draw(color=colors[color_number])
+#
+#     second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 90, length=200, width=3)
+#     second_line.draw(color=colors[color_number])
+#
+#     third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 180, length=200, width=3)
+#     third_line.draw(color=colors[color_number])
+#
+#     sd.line(start_point=third_line.end_point, end_point=square_point, width=3, color=colors[color_number])
+#
+#
+# def pentagon(pentagon_point, angle=0):
+#     first_line = sd.get_vector(start_point=pentagon_point, angle=0, length=150, width=3)
+#     first_line.draw(color=colors[color_number])
+#
+#     second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 72, length=150, width=3)
+#     second_line.draw(color=colors[color_number])
+#
+#     third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 144, length=150, width=3)
+#     third_line.draw(color=colors[color_number])
+#
+#     fourth_line = sd.get_vector(start_point=third_line.end_point, angle=angle + 216, length=150, width=3)
+#     fourth_line.draw(color=colors[color_number])
+#
+#     sd.line(start_point=fourth_line.end_point, end_point=pentagon_point, width=3,
+#             color=colors[color_number])
+#
+#
+# def hexagon(hexagon_point, angle=0):
+#     first_line = sd.get_vector(start_point=hexagon_point, angle=0, length=125, width=3)
+#     first_line.draw(color=colors[color_number])
+#
+#     second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 60, length=125, width=3)
+#     second_line.draw(color=colors[color_number])
+#
+#     third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 120, length=125, width=3)
+#     third_line.draw(color=colors[color_number])
+#
+#     fourth_line = sd.get_vector(start_point=third_line.end_point, angle=angle + 180, length=125, width=3)
+#     fourth_line.draw(color=colors[color_number])
+#
+#     fifth_line = sd.get_vector(start_point=fourth_line.end_point, angle=angle + 240, length=125, width=3)
+#     fifth_line.draw(color=colors[color_number])
+#
+#     sd.line(start_point=fifth_line.end_point, end_point=hexagon_point, width=3, color=colors[color_number])
+#
+#
+# colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN,
+#           sd.COLOR_BLUE, sd.COLOR_PURPLE)
+#
+# print('Возможные цвета', '\n', '0 : red', '\n', '1 : orange', '\n', '2 : yellow', '\n', '3 : green', '\n',
+#       '4 : cyan', '\n', '5 : blue', '\n', '6 : purple')
+#
+# while True:
+#     user_input = input("Введите желаемый цвет ")
+#     color_number = int(user_input)
+#     if color_number <= len(colors) - 1:
+#         break
+#     else:
+#         print('Введен некорректный номер')
+#
+# length = 200
+# point = sd.get_point(400, 400)
+# start_point_triangle_x = point.x + 150
+# start_point_triangle_y = point.y + 150
+# triangle_point = sd.get_point(start_point_triangle_x, start_point_triangle_y)
+# start_point_square_x = point.x - 150
+# start_point_square_y = point.y + 150
+# square_point = sd.get_point(start_point_square_x, start_point_square_y)
+# start_point_pentagon_x = point.x - 150
+# start_point_pentagon_y = point.y - 150
+# pentagon_point = sd.get_point(start_point_pentagon_x, start_point_pentagon_y)
+# start_point_hexagon_x = point.x + 150
+# start_point_hexagon_y = point.y - 150
+# hexagon_point = sd.get_point(start_point_hexagon_x, start_point_hexagon_y)
+#
+# triangle(triangle_point)
+# square(square_point)
+# pentagon(pentagon_point)
+# hexagon(hexagon_point)
 
-    second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 120, length=200, width=3)
-    second_line.draw(color=colors[color_number])
-
-    sd.line(start_point=second_line.end_point, end_point=triangle_point, width=3, color=colors[color_number])
+# -*- coding: utf-8 -*-
 
 
-def square(square_point, angle=0):
-    first_line = sd.get_vector(start_point=square_point, angle=0, length=200, width=3)
-    first_line.draw(color=colors[color_number])
+import simple_draw as sd
 
-    second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 90, length=200, width=3)
-    second_line.draw(color=colors[color_number])
+sd.set_screen_size(900, 900)
 
-    third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 180, length=200, width=3)
-    third_line.draw(color=colors[color_number])
-
-    sd.line(start_point=third_line.end_point, end_point=square_point, width=3, color=colors[color_number])
-
-
-def pentagon(pentagon_point, angle=0):
-    first_line = sd.get_vector(start_point=pentagon_point, angle=0, length=150, width=3)
-    first_line.draw(color=colors[color_number])
-
-    second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 72, length=150, width=3)
-    second_line.draw(color=colors[color_number])
-
-    third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 144, length=150, width=3)
-    third_line.draw(color=colors[color_number])
-
-    fourth_line = sd.get_vector(start_point=third_line.end_point, angle=angle + 216, length=150, width=3)
-    fourth_line.draw(color=colors[color_number])
-
-    sd.line(start_point=fourth_line.end_point, end_point=pentagon_point, width=3,
-            color=colors[color_number])
-
-
-def hexagon(hexagon_point, angle=0):
-    first_line = sd.get_vector(start_point=hexagon_point, angle=0, length=125, width=3)
-    first_line.draw(color=colors[color_number])
-
-    second_line = sd.get_vector(start_point=first_line.end_point, angle=angle + 60, length=125, width=3)
-    second_line.draw(color=colors[color_number])
-
-    third_line = sd.get_vector(start_point=second_line.end_point, angle=angle + 120, length=125, width=3)
-    third_line.draw(color=colors[color_number])
-
-    fourth_line = sd.get_vector(start_point=third_line.end_point, angle=angle + 180, length=125, width=3)
-    fourth_line.draw(color=colors[color_number])
-
-    fifth_line = sd.get_vector(start_point=fourth_line.end_point, angle=angle + 240, length=125, width=3)
-    fifth_line.draw(color=colors[color_number])
-
-    sd.line(start_point=fifth_line.end_point, end_point=hexagon_point, width=3, color=colors[color_number])
-
+point = sd.get_point(400, 400)
 
 colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN,
           sd.COLOR_BLUE, sd.COLOR_PURPLE)
@@ -91,24 +134,53 @@ while True:
     else:
         print('Введен некорректный номер')
 
+def figures(start_point=point, angle=0, length=200, width=3):
+    line = sd.get_vector(start_point, angle, length, width)
+    line.draw(color=colors[color_number])
+    return line.end_point
+
+
+def shapes_draw(start_point, end_point, next_angle=0, length=200, width=3, angle_step=0):
+    for angle in range(0, 360 - angle_step, angle_step):
+        start_point = figures(start_point, angle, length, width)
+    else:
+        sd.line(start_point=start_point, end_point=end_point, color=colors[color_number], width=3)
+
+
 length = 200
-point = sd.get_point(400, 400)
-start_point_triangle_x = point.x + 150
-start_point_triangle_y = point.y + 150
-triangle_point = sd.get_point(start_point_triangle_x, start_point_triangle_y)
-start_point_square_x = point.x - 150
-start_point_square_y = point.y + 150
-square_point = sd.get_point(start_point_square_x, start_point_square_y)
-start_point_pentagon_x = point.x - 150
-start_point_pentagon_y = point.y - 150
-pentagon_point = sd.get_point(start_point_pentagon_x, start_point_pentagon_y)
+
 start_point_hexagon_x = point.x + 150
 start_point_hexagon_y = point.y - 150
-hexagon_point = sd.get_point(start_point_hexagon_x, start_point_hexagon_y)
+start_point_pentagon_x = point.x - 150
+start_point_pentagon_y = point.y - 150
+start_point_square_x = point.x - 150
+start_point_square_y = point.y + 150
+start_point_triangle_x = point.x + 150
+start_point_triangle_y = point.y + 150
 
-triangle(triangle_point)
-square(square_point)
-pentagon(pentagon_point)
-hexagon(hexagon_point)
+triangle_point = sd.get_point(start_point_triangle_x, start_point_triangle_y)
+hexagon_point = sd.get_point(start_point_hexagon_x, start_point_hexagon_y)
+pentagon_point = sd.get_point(start_point_pentagon_x, start_point_pentagon_y)
+square_point = sd.get_point(start_point_square_x, start_point_square_y)
+triangle_angle = 120
+square_angle = 90
+pentagon_angle = 72
+hexagon_angle = 60
+
+shapes_draw(start_point=triangle_point, end_point=triangle_point, next_angle=triangle_angle, length=length, width=3,
+            angle_step=triangle_angle)
+shapes_draw(start_point=square_point, end_point=square_point, next_angle=square_angle, length=length, width=3,
+            angle_step=square_angle)
+shapes_draw(start_point=pentagon_point, end_point=pentagon_point, next_angle=pentagon_angle, length=150, width=3,
+            angle_step=pentagon_angle)
+shapes_draw(start_point=hexagon_point, end_point=hexagon_point, next_angle=hexagon_angle, length=125, width=3,
+            angle_step=hexagon_angle)
+
+
+
+
+
+sd.pause()
+
 
 sd.pause()
