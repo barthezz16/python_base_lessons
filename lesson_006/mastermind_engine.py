3e62561f03f3f2bd0310e4a20c0eaa85e8a496a4
bi4ok <bi4ok23@gmail.com>
2020-03-07 10:35:32 +0300

    lesson 006: нужны правки в 01, 02

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -9,24 +9,28 @@ from random import randint
 
 def make_number(digits_amount=4):
     global _numbers
-    _numbers = [randint(0, 9) for n in range(digits_amount)] # TODO почему эта переменная создается как кортеж а не список
-    print(_numbers)
+    _numbers = [randint(0, 9) for n in range(digits_amount)]
+    # почему эта переменная создается как кортеж а не список
+    print(type(_numbers), _numbers)  # TODO Не понимаю о чём вы, эта переменная является списком
 
 
 def compare_numbers(player_try):
     if player_try == _numbers:
-        print('Вы выиграли')
+        print('Вы выиграли')  # TODO Не нужно проверять частный случай победы,
+        # TODO Эта функция нужна только для подсчета быков и коров,
+        # TODO А уже в главном модуле будет проверка количества быков и выведение результата
     else:
         cows = 0
         bulls = 0
+        print(player_try, 'player_try')
+        print(_numbers, '_numbers')
         for x in range(0, digits_amount):
-            if player_try[x] == _numbers[x]: # TODO похоже изза этого и не считается количество быклов и коров
+            if player_try[x] == _numbers[x]:  # похоже изза этого и не считается количество быклов и коров
                 bulls += 1
             elif player_try[x] in _numbers:
                 cows += 1
-            return cows, bulls
-        print(player_try, 'player_try')
-        print(_numbers, '_numbers')
+            return cows, bulls  # TODO Не считается потому что происходит return на первой же итерации
+        # TODO возвращать результат надо после цикла, а не в нём (следите за отступом)
 
 
 _numbers = ()
--- lesson_006/snowfall_engine.py
+++ lesson_006/snowfall_engine.py
@@ -17,26 +17,25 @@ def snowflake_creating(snowflake_count):
 
 
 def snowflake_color(snowflake_count, color=sd.COLOR_WHITE):
-    # TODO В этой функции должно быть рисование, а остальное не нужно
-    # TODO Тут должен быть цикл и рисование snowflake. Создание списка тут не нужно.
-    # TODO Эта функция будет вызываться для отрисовки снежинок, поэтому snowflake_creating(snowflake_count)
-    # TODO будет каждый раз обнулять список.
+    # TODO В этой функции должен быть только цикл по снежинкам и функция рисования snowflake
+    # TODO move_snowflake - удалить
     move_snowflake(3, 5)
     sd.snowflake(center=point, length=snowflake_size[i], color=sd.COLOR_WHITE)
     for n in enumerate(y_list):
         move_snowflake(3, 5)
 
 
-def snowflake_deleting():  # что то примерное, но создается всего одна
-    if y_list[i] < 10:  # TODO Здесь надо циклом проходить по списку, проверять, упала ли снежинка
-        # TODO и добавлять её индекс в список, а потом этот список ретурном возвращать
+def snowflake_deleting():
+    # TODO Здесь цикл по снежинкам, проверка 'y' координаты, добавление этой координаты в новый список
+    # TODO Обновлять координату не нужно
+    if y_list[i] < 10:
         y_list[i] = 800
     return y_list[i]
 
 
 def move_snowflake(x_position=3, y_position=5):
-    # TODO Здесь нужно оставить только изменение координат
-    # TODO Рисование и прочее тут не нужно, только пройти по спискам и изменить координаты
+    # TODO Здесь цикл по снежинкам, с изменением координат (42, 43 строка)
+    # TODO создание точек, snowflake_deleting и sd.snowflake - удалить
     global point
     for i, y in enumerate(y_list):
         point = sd.get_point(x_list[i], y_list[i])
