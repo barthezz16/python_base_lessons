5d2abe80af71d666cc77c51e9936fc65ec5c4e82
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-09 20:13:08 -0400

    02_snowfall_module.py

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -17,38 +17,22 @@ sd.resolution = (1200, 800)
 
 # создать_снежинки(N)
 from snowfall_engine import snowflake_creating, snowflake_color, move_snowflake, snowflake_deleting, \
-    new_snowflake_creating, index_list
+    new_snowflake_creating, remove_snowflake
 
 snowflake_numbers = 50
 snowflake_creating(snowflake_numbers)
 count = 0
+flake_del = []
 while True:
     sd.start_drawing()
     snowflake_color(color=sd.background_color)
     move_snowflake(3, 6)
     snowflake_color(color=sd.COLOR_WHITE)
-    # TODO лучше всего переместить сюда index_list = snowflake_deleting()
-    # TODO А затем уже проверять index_list
-    # TODO И импортировать его не нужно. Он импортируется пустым, и мы его никак не пополняем
-    # TODO тк в snowflake_deleting() не прописано, что index_list глобальный
-    # TODO без global он создается внутри функции независимо и никак не влияет на index_list, который написан в начале
+    index_list = snowflake_deleting()
     if index_list:
-        count += 1
-        snowflake_deleting()  # TODO эта функция создает список индексов удаленных снежинок
-        # TODO этот список надо сохранить в переменной и передать функции remove_snowflake
-        # TODO длину этого списка можно использовать вместо count
-        # TODO сам count будет немного ошибаться:
-        # TODO если упадут две или больше снежинки он добавит только +1
-        # TODO кроме того, count будет с каждым кругом добавлять всё больше и больше снежинок
-        # TODO В целом, если вы это и планировали - можно оставить count
-        # TODO А можно использовать длину списка, который возвращает snowflake_deleting()
-        # список = snowflake_deleting()
-        # remove_snowflake(список)
-        # new_snowflake_creating(len(список)) -- тогда добавится столько снежинок, сколько было удалено
-        new_snowflake_creating(count)
-    #  если есть номера_достигших_низа_экрана() то
-    #       удалить_снежинки(номера)  # честно, тут вообще не понял что должно быть...
-    #       создать_снежинки(count)
+        remove_snowflake(index_list)
+        new_snowflake_creating(len(index_list)) # TODO и опять что то не то я делаю, вроде как уже заработало,
+        # TODO но почему то появляются рандомные статичные снежинки и те которые падают как надо...
     sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
--- lesson_006/snowfall_engine.py
+++ lesson_006/snowfall_engine.py
@@ -17,8 +17,8 @@ def snowflake_creating(snowflake_count):
     y_list = []
     snowflake_size = []
     for i in range(snowflake_count):
-        x_list.append(sd.random_number(25, 1000))
-        y_list.append(sd.random_number(100, 780))
+        x_list.append(sd.random_number(25, 1170))
+        y_list.append(sd.random_number(0, 800))
         snowflake_size.append(sd.random_number(15, 35))
 
 
@@ -32,7 +32,7 @@ def snowflake_color(color=sd.COLOR_WHITE):
 def snowflake_deleting():
     index_list = []
     for i, y in enumerate(y_list):
-        if y < 20:
+        if y < -36:
             index_list.append(i)
     return index_list
 
@@ -44,33 +44,13 @@ def move_snowflake(x_position=3, y_position=5):
 
 
 def remove_snowflake(index_list):
+    index_list = index_list[::-1]
     for i in index_list:
-        del index_list[-1]  # TODO Удалять надо из списков x_list, y_list, snowflake_size
-        # TODO тут вместо -1 надо использовать индексы из index_list, т.е. подставлять вместо -1 - i
-        # TODO В index_list записаны номера снежинок, которые надо удалить
-        # TODO циклом мы проходим по списку, по-очереди номера передаются в i
-        # Пример работы цикла изнутри:
-        # index_list = [1, 2, 3]  -- создается список index_list, ему присваивается то, что передали параметром
-        # i = 1  -- начинается первая итерация цикла
-        # del x_list[i]
-        # del y_list[i]
-        # del snowflake_size[i]
-        # i = 2
-        # del x_list[i]
-        # del y_list[i]
-        # del snowflake_size[i]
-        # i = 3
-        # del x_list[i]
-        # del y_list[i]
-        # del snowflake_size[i]
-        # TODO Перевернуть список можно при помощи среза список[::-1]
-        # TODO И это желательно сделать до цикла
-        # TODO index_list = index_list[::-1]
-        # TODO и далее уже запускать цикл по индекс_лиск и удалять снежинки из 3 списков с координатами и размером
+        del x_list[-i], y_list[-i], snowflake_size[-i]
 
 
 def new_snowflake_creating(snowflake_count):
     for i in range(snowflake_count):
         x_list.append(sd.random_number(25, 1000))
-        y_list.append(sd.random_number(100, 780))
+        y_list.append(sd.random_number(0, 1200))
         snowflake_size.append(sd.random_number(15, 35))
