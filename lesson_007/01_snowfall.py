928167eafa60326da55bfd241232772ae527a7e2
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-17 16:56:46 -0400

    готово 01_snowfall.py

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -15,7 +15,7 @@ class Snowflake:
 
     def __init__(self):
         self.x = (sd.random_number(25, 1170))
-        self.y = sd.random_number(500, 800)
+        self.y = sd.random_number(700, 1000)
         self.snowflake_size = sd.random_number(15, 35)
         self.index_list = []
 
@@ -23,7 +23,7 @@ class Snowflake:
         point = sd.get_point(self.x, self.y)
         sd.snowflake(center=point, length=self.snowflake_size, color=color)
 
-    def move(self, x_position=2, y_position=7):
+    def move(self, x_position=2, y_position=4):
         self.x -= sd.random_number(-x_position, x_position)
         self.y -= y_position
 
@@ -54,13 +54,13 @@ def get_fallen_flakes():
 
 def append_flakes(count):
     for i in range(count):
-        flake_list.append(Snowflake())
-    return fallen_flakes  # TODO Зачем тут возврат fallen_flakes?
+        flake_list.append(Snowflake()) # TODO а тут согласен... мы же просто создаем новые объекты,
+        # TODO ничего не надо возвращать
 
 
 flake_list = []
 flake = Snowflake()
-flakes = get_flakes(count=30)
+flakes = get_flakes(count=50)
 while True:
     for flake in flakes:
         flake.clear_previous_picture()
@@ -68,9 +68,10 @@ while True:
         flake.draw()
     fallen_flakes = get_fallen_flakes()
     if fallen_flakes:
-        append_flakes(count=fallen_flakes)  # TODO Ошибка тут
-        # TODO Функция принимает на вход список, а должна количество удаленных снежинок получать
-    sd.sleep(0.1)
+        append_flakes(count=len(fallen_flakes))  # TODO вот тут как раз я и не могу понять,
+        # TODO просто тут так было изначально написано что принимает список,
+        # TODO вот я и думал что надо под этот код подстроить и не понял как...
+    sd.sleep(0.05)
     if sd.user_want_exit():
         break
 
