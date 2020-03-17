233f1fd85d48e31aa0e25e002ffead4c5fa16377
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-16 20:02:01 -0400

    любимые снежинки

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -2,6 +2,9 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 800)
+
+
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,9 +12,34 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    def __init__(self):
+        self.x_list = []
+        self.y_list = []
+        self.snowflake_size = []
+        self.index_list = []
+        self.x_list.append(sd.random_number(25, 1170))
+        self.y_list.append(sd.random_number(0, 800))
+        self.snowflake_size.append(sd.random_number(15, 35))
+
+    def draw(self, color=sd.COLOR_WHITE):
+        for i, y in enumerate(self.y_list):
+            point = sd.get_point(self.x_list[i], self.y_list[i])
+            sd.snowflake(center=point, length=self.snowflake_size[i], color=color)
+
+    def move(self, x_position=3, y_position=5):
+        for i, y in enumerate(self.y_list):
+            self.x_list[i] -= sd.random_number(-x_position, x_position)
+            self.y_list[i] -= y_position
+
+    def can_fall(self):
+        for i, y in enumerate(self.y_list):
+            if y < 20:
+                self.index_list.append(i)
+        return self.index_list
+
+    def clear_previous_picture(self):
+        sd.clear_screen()
 
 
 flake = Snowflake()
