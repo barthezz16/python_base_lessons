6002a6039eb28585e40816b319575415e77089b7
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-17 15:30:40 -0400

    процесс 01_snowfall.py

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -36,36 +36,30 @@ class Snowflake:
 
 
 def get_flakes(count):
-    flake = Snowflake()
     for i in range(count):
-        flake_list.append(flake)  # TODO вместо flake нужно сюда поместить Snowflake()
-        # TODO Snowflake() - срабатывает и создает объект класса
-        # TODO Если его вызвать до цикла, то вы добавите в список много одинаковых объектов
-        # TODO А если внутри цикла вызывать Snowflake() то каждый раз будет создаваться новый объект-снежинка
-        return flake_list  # не пойму, почему не добавляются снижинки в список...
-        # TODO Обратите внимание на отступ, return срабатывает сразу же на первой итерации цикла
-    # TODO Обрывает его выполнение и завершает функцию, добавив всего одну снежинку
+        flake_list.append(Snowflake())
+    return flake_list
+
 
 def get_fallen_flakes():
     fallen_flakes = []
-    # TODO Тут нужно пройти по списку снежинок и записывать их индексы в списке, а не сами снежинки
-    if flake.y < -36:  # TODO Используйте тут метод снежинок can_fall, только с приставкой not
-        fallen_flakes.append(flake)
+    for i in enumerate(flake_list):
+        if not flake.can_fall():
+            fallen_flakes.append(i)
     return fallen_flakes
 
 
 def append_flakes(count):
-    # TODO И тут нужен цикл. И вообще функция должна принимать список параметром - изменять его - возвращать ретурном
-    flake.x = (sd.random_number(25, 1170))
-    flake.y = sd.random_number(500, 800)
-    flake.snowflake_size = sd.random_number(15, 35)
+    for i in range(fallen_flakes):
+        flake.x = (sd.random_number(25, 1170))
+        flake.y = sd.random_number(500, 800)
+        flake.snowflake_size = sd.random_number(15, 35)
+    return fallen_flakes
 
 
 flake_list = []
 flake = Snowflake()
-N = 30
-flakes = get_flakes(count=N)
-print(len(flakes))
+flakes = get_flakes(count=30)
 while True:
     for flake in flakes:
         flake.clear_previous_picture()
