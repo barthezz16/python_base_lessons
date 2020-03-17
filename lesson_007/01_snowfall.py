3bd6bd40091c13cf38cc3e9f0f6ba592e2c743d9
bi4ok <bi4ok23@gmail.com>
2020-03-17 22:04:10 +0300

    lesson 007: нужны правки в 01

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -38,18 +38,24 @@ class Snowflake:
 def get_flakes(count):
     flake = Snowflake()
     for i in range(count):
-        flake_list.append(flake)
-        return flake_list # TODO не пойму, почему не добавляются снижинки в список...
-
+        flake_list.append(flake)  # TODO вместо flake нужно сюда поместить Snowflake()
+        # TODO Snowflake() - срабатывает и создает объект класса
+        # TODO Если его вызвать до цикла, то вы добавите в список много одинаковых объектов
+        # TODO А если внутри цикла вызывать Snowflake() то каждый раз будет создаваться новый объект-снежинка
+        return flake_list  # не пойму, почему не добавляются снижинки в список...
+        # TODO Обратите внимание на отступ, return срабатывает сразу же на первой итерации цикла
+    # TODO Обрывает его выполнение и завершает функцию, добавив всего одну снежинку
 
 def get_fallen_flakes():
     fallen_flakes = []
-    if flake.y < -36:
+    # TODO Тут нужно пройти по списку снежинок и записывать их индексы в списке, а не сами снежинки
+    if flake.y < -36:  # TODO Используйте тут метод снежинок can_fall, только с приставкой not
         fallen_flakes.append(flake)
     return fallen_flakes
 
 
 def append_flakes(count):
+    # TODO И тут нужен цикл. И вообще функция должна принимать список параметром - изменять его - возвращать ретурном
     flake.x = (sd.random_number(25, 1170))
     flake.y = sd.random_number(500, 800)
     flake.snowflake_size = sd.random_number(15, 35)
