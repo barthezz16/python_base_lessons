07425d88fc33a6e67dcd71a5459606a50c6f4b43
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-26 12:15:20 -0400

    процесс 01_char_stat.py

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -20,8 +20,44 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import zipfile
 
-# TODO здесь ваш код
+
+class Counter:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect_stat(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for char in line[:-1]:
+                    if char in self.stat:
+                        self.stat[char] += 1
+                    else:
+                        self.stat[char] = 1
+            print('+{txt:-^21}+'.format(txt='+'))
+            print('|{txt:^10}|'.format(txt='Буква') + '{txt:^10}|'.format(txt='Частота'))
+            print('+{txt:-^21}+'.format(txt='+'))
+            for key in sorted(self.stat):
+                if key.isalpha():
+                    print(f'|{key:^10}|' + f'{self.stat.get(key):^10}|')
+        print('+{txt:-^21}+'.format(txt='+'))
+        print('|{txt:^10}|'.format(txt='Итого') + '{txt:^10}|'.format(txt=sum(self.stat.values())))
+        print('+{txt:-^21}+'.format(txt='+'))
+
+
+counter = Counter(file_name='voyna-i-mir.txt.zip')
+counter.collect_stat()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
