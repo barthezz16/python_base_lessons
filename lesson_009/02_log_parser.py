d858dcee40f282b2246a6711e2d91c313879e7f5
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-26 15:47:48 -0400

    првки в 01_char_stat.py и 02_log_parser.py

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -27,22 +27,39 @@ class LogParser:
         self.stat = {}
         self.item_to_find = 'NOK'
 
-    def stat_collector(self):
+    def stat_collector_by_minutes(self):
         with open(self.file_name, mode='r') as file:
             for line in file:
                 if self.item_to_find in line:
                     if str(line[1:-16]) in self.stat:
                         self.stat[str(line[1:-16])] += 1
-                        self.print(line)
+                        return line
                     else:
                         self.stat[str(line[1:-16])] = 1
 
-    def print(self, line):
+    def stat_collector_by_hours(self):
+        with open(self.file_name, mode='r') as file:
+            for line in file:
+                if self.item_to_find in line:
+                    if str(line[1:-19]) in self.stat:
+                        self.stat[str(line[1:-19])] += 1
+                        return line
+                    else:
+                        self.stat[str(line[1:-19])] = 1
+
+    def print_by_minutes(self, line):
         print(f'[{str(line[1:-16]):^16}]' + f'{self.stat.get(str(line[1:-16])):^5}')
+
+    def print_by_hours(self, line):
+        print(f'[{str(line[1:-16]):^12}]' + f'{self.stat.get(str(line[1:-16])):^5}')
 
 
 parser = LogParser(file_name='events.txt')
-parser.stat_collector()
+minutes_line = parser.stat_collector_by_minutes()
+parser.print_by_minutes(line=minutes_line)
+hours_line = parser.stat_collector_by_minutes()
+parser.print_by_hours(line=hours_line)
+
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
