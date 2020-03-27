3e27627802ce7dbf240cd39781af02ffdadc05ea
Юрий Сорокин <ibarthezz@gmail.com>
2020-03-27 11:52:19 -0400

    правки в 02_log_parser.py

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -26,50 +26,36 @@ class LogParser:
         self.file_name = file_name
         self.stat = {}
         self.item_to_find = 'NOK'
+        self.line = None
 
-    def stat_collector_by_minutes(self):
+    def stat_collector(self, param):
         with open(self.file_name, mode='r') as file:
             for line in file:
                 if self.item_to_find in line:
-                    if str(line[1:-16]) in self.stat:
-                        self.stat[str(line[1:-16])] += 1
-                        return line  # TODO это ведь завершит проход по файлу
-                    # TODO В итоге будет получены только данные за одну минуту и то не до конца
-                    # TODO Тут 2 варианта:
-                    # 1) сперва всё записать в self.stat и затем вывести это отдельно(как было в 01_char_stat)
-                    # 2) выводить данные принтом по каждой строке, НО в момент, когда будет найдена новая строка
-                    # Этот вариант сложнее, тк придётся придумать, как сохранять прошлое значение и строку
+                    if str(line[1:-param]) in self.stat:
+                        self.stat[str(line[1:-param])] += 1
                     else:
-                        # Кстати новые минуты попадают сперва сюда
-                        # Значит и печатать надо будет результат отсюда
-                        self.stat[str(line[1:-16])] = 1
+                        self.stat[str(line[1:-param])] = 1
 
-    # TODO Этот метод почти дублирует предыдущий. Разница лишь в одном числе (вместо -16 тут -19)
-    # TODO Это число можно сделать параметром(или лучше атрибутом).
-    # TODO Тогда можно будет сделать под каждую сортировку метод, который будет изменять атрибут и запускать сортировку
-    def stat_collector_by_hours(self):
-        with open(self.file_name, mode='r') as file:
-            for line in file:
-                if self.item_to_find in line:
-                    if str(line[1:-19]) in self.stat:
-                        self.stat[str(line[1:-19])] += 1
-                        return line
-                    else:
-                        self.stat[str(line[1:-19])] = 1
+    def print_stat(self):
+        for i in self.stat:
+            print(i, self.stat[i])
 
-    def print_by_minutes(self, line):
-        print(f'[{str(line[1:-16]):^16}]' + f'{self.stat.get(str(line[1:-16])):^5}')
 
-    def print_by_hours(self, line):
-        print(f'[{str(line[1:-19]):^12}]' + f'{self.stat.get(str(line[1:-19])):^5}')
-
+param_for_minutes = 16
+param_for_hours = 19
+param_for_mounth = 25
+param_for_year = 28
 
 parser = LogParser(file_name='events.txt')
-minutes_line = parser.stat_collector_by_minutes()
-parser.print_by_minutes(line=minutes_line)
-hours_line = parser.stat_collector_by_hours()
-parser.print_by_hours(line=hours_line)
-
+parser.stat_collector(param_for_minutes)
+parser.print_stat()
+parser.stat_collector(param_for_hours)
+parser.print_stat()
+parser.stat_collector(param_for_mounth)
+parser.print_stat()
+parser.stat_collector(param_for_year)
+parser.print_stat()
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
