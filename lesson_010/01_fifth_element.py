05013959d01d14ea78604f0caf80ebeeeca41dba
Юрий Сорокин <ibarthezz@gmail.com>
2020-04-02 18:58:51 -0400

    процесс 01_fifth_element.py

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,17 +4,20 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError:
+    print('невозможно преобразовать к числу')
+except IndexError:
+    print('выход за границы списка')
+except:
+    print('неправильный ввод')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
