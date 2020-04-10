7f93c2d622445cfbe08ae8d3c8be70a1c162bbfa
Юрий Сорокин <ibarthezz@gmail.com>
2020-04-10 11:52:40 -0400

    правки 01_shapes.py разобрался в правках

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -33,6 +33,21 @@ def get_polygon(n):
 
 
 draw_triangle = get_polygon(n=3)
-triangle_point = sd.get_point(400, 400)
+triangle_point = sd.get_point(600, 600)
 draw_triangle(start_point=triangle_point, end_point=triangle_point, next_angle=0, length=200, width=3)
+
+
+draw_square = get_polygon(n=4)
+square_point = sd.get_point(200, 600)
+draw_square(start_point=square_point, end_point=square_point, next_angle=0, length=200, width=3)
+
+
+draw_pentagon = get_polygon(n=5)
+pentagon_point = sd.get_point(200, 200)
+draw_pentagon(start_point=pentagon_point, end_point=pentagon_point, next_angle=0, length=200, width=3)
+
+
+draw_hexagon = get_polygon(n=6)
+hexagon_point = sd.get_point(600, 200)
+draw_hexagon(start_point=hexagon_point, end_point=hexagon_point, next_angle=0, length=150, width=3)
 sd.pause()
