b64d8293e836da7ecfeb2e0f07d37e6c8123e136
Юрий Сорокин <ibarthezz@gmail.com>
2020-04-15 12:59:01 -0400

    процесс 02_volatility_with_threads.py

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -17,6 +17,61 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
+import os
+import threading
 
-# TODO тут ваш код в многопоточном стиле
+from Utils import sort_and_print, total, time_track
+
+
+class VolatilityAnalyser(threading.Thread):
+
+    def __init__(self, file_to_read, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_to_read = file_to_read
+        self.files_to_open = None
+        self.data = {}
+        self.price_list = []
+        self.value = []
+        self.average_price = 0
+        self.volatility = 0
+        self.secid = 0
+        self.tradetime = 0
+        self.price = 0
+        self.quantity = 0
+        self.result = {}
+        self.zero_volatility = []
+        self.min_volatility = {}
+        self.max_volatility = {}
+
+    def run(self,):
+        self.file_analytics(self.file_to_read)
+
+    def file_analytics(self, file_to_read):
+        with open(os.path.join('trades', file_to_read), 'r') as file:
+            # analysers = [VolatilityAnalyser(file_to_read=line) for line in file.readlines()[1:]]
+            # for analyser in analysers: # TODO вот так уже все работает, но когда я бытаюсь разбить по потокам
+            #     analyser.start()  # TODO чтнение строк, все ломается, line из analysers не видна ниже
+            # for analyser in analysers: # TODO где я хочу ее распаковать
+            #     analyser.join()
+            for line in file.readlines()[1:]:
+                self.secid, self.tradetime, self.price, self.quantity = line.split(',')
+                self.price_list.append((float(self.price)))
+                self.data[self.secid] = self.price_list
+            self.value = list(self.data.values())
+            self.average_price = (min(self.value[0]) + max(self.value[0])) / 2
+            self.volatility = ((max(self.value[0]) - min(self.value[0])) / self.average_price) * 100
+            self.result[self.secid] = self.volatility
+            total.update(self.result)
+
+@time_track
+def get_file():
+
+        analysers = [VolatilityAnalyser(file_to_read=files)for files in os.listdir('trades')]
+        for analyser in analysers:
+            analyser.start()
+        for analyser in analysers:
+            analyser.join()
+
+get_file()
+sort_and_print()
+
