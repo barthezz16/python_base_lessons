16cf2bb91d8a006594de90b82b4450a4a37d39bf
Юрий Сорокин <ibarthezz@gmail.com>
2020-04-20 19:08:46 -0400

    процесс 01_score.py

--- lesson_014/01_score.py
+++ lesson_014/01_score.py
@@ -31,10 +31,86 @@
 # Из текущего файла сделать консольную утилиту для определения количества очков, с помощью пакета argparse
 # Скрипт должен принимать параметр --result и печатать на консоль:
 #   Количество очков для результатов ХХХ - УУУ.
+import argparse
+from random import randint
 
-# TODO тут ваш код
+from termcolor import cprint
+
+PINS = 10
+FRAME = 10
+
+
+# class Bowling:
+#
+#     def __init__(self):
+#         self.pins = 10
+#         self.remaining_pins = 0
+#         self.score = 0
+#
+#     def run(self):
+#         total_score = []
+#         for _ in range(FRAME):
+#             cprint('*' * 45, color='green')
+#             cprint(f'Фрейм # {_ + 1}', attrs=['bold'], color='yellow')
+#             try_result = []
+#             self.remaining_pins = PINS
+#             throw_bowling_ball_try = 1
+#             self.throw_ball(throw_bowling_ball_try, total_score, try_result)
+#
+#     def throw_ball(self, throw_bowling_ball_try, total_score, try_result):
+#         while self.remaining_pins > 0 <= 10 and throw_bowling_ball_try <= 2:
+#             throw = randint(0, self.remaining_pins)
+#             self.remaining_pins -= throw
+#             throw_bowling_ball_try += 1
+#             print('Бросок #', throw_bowling_ball_try,
+#                   'сбито кеглей', throw if throw > 0 else '-',
+#                   'осталось кеглей', self.remaining_pins,
+#                   )
+#             if throw == 10:
+#                 try_result.append(20)
+#                 cprint('***!STRIKE!***', color='red')
+#             elif self.remaining_pins == 0:
+#                 try_result.append(15 - try_result[0])
+#                 cprint('SPARE', color='red')
+#             else:
+#                 try_result.append(throw)
+#         self.get_score(total_score, try_result)
+#
+#     def get_score(self, total_score, try_result):
+#         total_score.append(sum(try_result))
+#         print('всего очков', sum(total_score))
+#
+#
+# game = Bowling()
+# game.run()
+
+
+def bowling(total_score):
+    total_score = []
+    for _ in range(FRAME):
+        try_result = []
+        remaining_pins = PINS
+        throw_bowling_ball_try = 1
+        while remaining_pins > 0 <= 10 and throw_bowling_ball_try <= 2:
+            throw = randint(0, remaining_pins)
+            remaining_pins -= throw
+            throw_bowling_ball_try += 1
+            if throw == 10:
+                try_result.append(20)
+            elif remaining_pins == 0:
+                try_result.append(15 - try_result[0])
+            else:
+                try_result.append(throw)
+        total_score.append(sum(try_result))
+    print('всего очков', sum(total_score))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('result', help='Show game result')
+args = parser.parse_args()
+
+bowling(args.result)
 
 # При написании кода помнить, что заказчик может захотеть доработок и новых возможностей...
 # И, возможно, вам пригодится паттерн проектирования "Состояние",
 #   см https://clck.ru/Fudd8 и https://refactoring.guru/ru/design-patterns/state
-
