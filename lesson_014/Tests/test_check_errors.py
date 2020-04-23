48b1cfe5c7d5f42b604921dd4e6c3145a07341e7
bi4ok <bi4ok23@gmail.com>
2020-04-23 20:53:32 +0300

    lesson 014: нужны правки в 02

--- lesson_014/Tests/test_check_errors.py
+++ lesson_014/Tests/test_check_errors.py
@@ -15,3 +15,7 @@ class MyTests(unittest.TestCase):
     def test_3(self):
         result = analyzing_result('9934-/1744XX23--4/')
         self.assertEqual(result, 'Введено неправильное значение, сумма одного фрейма больше 9 очков')
+        # TODO Для проверки ошибок есть подобный синтаксис
+        # with self.assertRaises(Exception) as err:
+        #     get_score(game_result)
+        # self.assertEqual("Неверное число ходов", str(err.exception))
--- lesson_014/bowling_engine.py
+++ lesson_014/bowling_engine.py
@@ -17,7 +17,6 @@ def analyzing_result(result):
     return total
 
 
-
 def result_count(v):
     global total
     if 'X' in v:
@@ -40,47 +39,6 @@ def check_errors(v):
         raise ValueError('Spare на первом броске')
     elif 'X' in v[1]:
         raise ValueError('Strike на втором броске')
-    if v[0].isdigit() and v[1].isdigit() and int(v[0]) + int(v[0]) >= 10:
+    if v[0].isdigit() and v[1].isdigit() and int(v[0]) + int(v[0]) >= 10:  # TODO вместо v[0] должно быть v[1]
         raise ValueError('Введено неправильное значение, сумма одного фрейма больше 9 очков')
 
-
-# try:
-#     result = 'X34-/1744XX23--4/'
-#     analyzing_result(result=result)
-# except Exception as exc:
-#     print(f'{result} --> {exc}')
-# try:
-#     result = '12X34-/1744XX23--4/'
-#     analyzing_result(result=result)
-# except Exception as exc:
-#     print(f'{result} -->  {exc}')
-# try:
-#     result = '1X4-/1744XX23--4/'
-#     analyzing_result(result=result)
-# except Exception as exc:
-#     print(f'{result} -->  {exc}')
-# try:
-#     result = '/34-/1744XX23--4/'
-#     analyzing_result(result=result)
-# except Exception as exc:
-#     print(f'{result} -->  {exc}')
-try:
-    result = '9934-/1744XX23--4/'
-    analyzing_result(result=result)
-except Exception as exc:
-    print(f'{result} -->  {exc}')
-# try:
-#     result = '0534-/1744XX23--4/'
-#     analyzing_result(result=result)
-# except Exception as exc:
-#     print(f'{result} -->  {exc}')
-
-# TODO Добавлю пример. Пусть есть строка result = 'X34-/1744XX23--4/'
-# TODO Напишите функцию(а лучше метод), который примет параметром эту строку, а на выходе выдаст
-# TODO {1:(X, -), 2:(3, 4), 3:(-, /), 4:(1, 7), 5:(4, 4), 6:(X, -), 7:(X, -), 8:(2, 3), 9:(-, -), 10:(4, /)}
-# TODO Тогда можно будет легко проверить строку на наличие ошибок.
-# TODO Примеры фреймов с ошибками: (1, X) - страйк на втором броске
-# TODO (/, 2) - / на первом броске
-# TODO (5, 5) - сумма очков больше 9 (в этом случае должен быть (5, /)
-# TODO (0, 2) - вместо 0 должен быть "-"
-# TODO Так же надо проверять количесвто фреймов + наличие других символов в строке.
