b75605f265445befbebb9addc1fa9d1bdcc92c2e
bi4ok <bi4ok23@gmail.com>
2020-05-11 11:34:35 +0300

    lesson 015: нужны правки в 01

--- lesson_015/test.py
+++ lesson_015/test.py
@@ -9,3 +9,22 @@ pprint(data["Location_0_tm0"]
        [-1]['Location_7_tm33300']
        [-1]['Location_10_tm55100']
        [-1]['Location_12_tm0.0987654320'][0])
+
+data = data['Location_0_tm0']
+mob_list = []
+loc_list = []
+# Собираем данные из локации
+for index, item in enumerate(data):
+    if isinstance(item, str):
+        mob_list.append(item)
+    elif isinstance(item, dict):
+        loc_list.append((list(item.keys())[0], index))
+# Переход в нужную локацию
+for index, name_loc in enumerate(loc_list):
+    print(f'{index + 1}.{name_loc[0]}')
+
+input_user = input('Сделайте выбор!')
+print(loc_list[int(input_user) - 1])
+key, index = loc_list[int(input_user) - 1]
+data = data[int(index)][key]
+print(data)
