960f1c5794787a7b741acb4832bcc4f508dea439
Юрий Сорокин <ibarthezz@gmail.com>
2020-06-06 21:30:51 -0400

    процесс weather

--- lesson_016/01_weather.py
+++ lesson_016/01_weather.py
@@ -46,3 +46,95 @@
 # Приконнектится по полученному url-пути к базе данных
 # Инициализировать её через DatabaseProxy()
 # https://peewee.readthedocs.io/en/latest/peewee/database.html#dynamically-defining-a-database
+from collections import OrderedDict
+from pprint import pprint
+from PIL import Image, ImageDraw
+from bs4 import BeautifulSoup
+import requests
+import cv2
+
+
+class WeatherMaker:
+    def __init__(self):
+        self.weather_dict = OrderedDict({})
+        self.response = requests.get(
+            'https://yandex.ru/pogoda/new-york?utm_campaign=informer&utm_content=main_informer&utm_medium'
+            '=web&utm_source=home&utm_term=main_number')
+
+    def get_weather(self):
+        if self.response.status_code == 200:
+            html_doc = BeautifulSoup(self.response.text, features='html.parser')
+            list_of_values = html_doc.find_all('span', {'class': 'temp__value'})
+            list_of_names = html_doc.find_all('div', {'class': 'forecast-briefly__name'})
+            list_of_weather = html_doc.find_all('div', {'class': 'forecast-briefly__condition'})
+            list_of_date = html_doc.find_all('time', {'class': 'time forecast-briefly__date'})
+            for names, values, weather, date in zip(list_of_names, list_of_values, list_of_weather, list_of_date):
+                self.weather_dict[str(date.text)] = str(values.text), str(weather.text)
+            return self.weather_dict
+
+
+weather_dict = WeatherMaker()
+pprint(weather_dict.get_weather())
+
+
+class ImageMaker:
+
+    def __init__(self):
+        self.background_image_cv = cv2.imread('python_snippets/external_data/probe1.jpg')
+        self.weather_dict_to_draw = WeatherMaker()
+        self.data = self.weather_dict_to_draw.get_weather()
+        self.image_pil = Image.open("python_snippets/external_data/probe.jpg")
+        self.pixels = self.image_pil.load()
+        self.draw = ImageDraw.Draw(self.image_pil)
+        self.width = self.image_pil.size[0]
+        self.height = self.image_pil.size[1]
+
+    def run(self):
+        self.get_data()
+        self.white_to_blue()
+        self.draw_card()
+        # self.view_image()
+
+    def white_to_blue(self):
+        for i in range(self.width, -1):
+            for j in range(self.height, -1):
+                self.draw.line((i, j), (i, i, i * 2))
+        self.image_pil.save('python_snippets/external_data/probe1.jpg')
+        # TODO пока тоже не совем понял как лучше градиент сделать
+
+    def get_data(self):
+        pass
+
+    def draw_card(self):
+        date = input('Дата: ')
+        cv2.putText(self.background_image_cv, 'New York',
+                    (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 0, 0), 2)
+        cv2.putText(self.background_image_cv, self.data[date][0],
+                    (0, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2) # TODO не совсем тут понял как мне
+        #  TODO использовать другой шрифт.
+        cv2.putText(self.background_image_cv, self.data[date][1],
+                    (0, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+        cv2.imwrite('python_snippets/external_data/probe1.jpg', self.background_image_cv)
+        self.background_image_cv = Image.open("python_snippets/external_data/probe1.jpg")
+        print(self.data[date][1])
+        if self.data[date][1] == 'Небольшой дождь' or 'Дождь':
+            img_to_paste = Image.open('python_snippets/external_data/weather_img/rain.png')
+        if self.data[date][1] == 'Облачно с прояснениями' or 'Малооблачно':
+            img_to_paste = Image.open('python_snippets/external_data/weather_img/cloud.png')
+        if self.data[date][1] == 'Ясно':
+            img_to_paste = Image.open('python_snippets/external_data/weather_img/sun.png')
+        if self.data[date][1] == 'Снег':
+            img_to_paste = Image.open('python_snippets/external_data/weather_img/snow.png')
+        self.background_image_cv.paste(img_to_paste, (400, 25))
+        self.background_image_cv.save('python_snippets/external_data/probe1.jpg', 'JPEG')
+
+    def view_image(self):
+        cv2.namedWindow('Picture', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Picture', self.background_image_cv)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# TODO про базу данных все помню, завтра этим займусь, этот процесс скидываю скорее чтобы проверить, правльно ли
+# TODO я двигаюсь
+image = ImageMaker()
+image.run()
